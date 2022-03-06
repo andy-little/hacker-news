29e3645eba99a6573ee8f4f830dde5eb539d4e78
andy-little <69594249+andy-little@users.noreply.github.com>
2022-03-06 12:08:55 +0000

    added basic pagination and header tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { screen, render } from "@testing-library/react";
+import { screen, render, waitFor } from "@testing-library/react";
 import App from "./App";
 import { AppProvider } from "./context";
 
@@ -21,10 +21,21 @@ describe("heading", () => {
     });
 });
 
-test("page one should not render previous button", () => {
-    render(<WrappedApp />);
-    const prevBtn = screen.queryByText(/previous/i);
-    expect(prevBtn).toBeNull();
+describe("pagination", () => {
+    test("page one should not render previous button", async () => {
+        render(<WrappedApp />);
+        await waitFor(() => {
+            const prevBtn = screen.queryByText(/previous page/i);
+            expect(prevBtn).not.toBeInTheDocument();
+        });
 
-    //expect(prevBtn).toBeInTheDocument();
+        //expect(prevBtn).toBeInTheDocument();
+    });
+    test("page one should render next button", async () => {
+        render(<WrappedApp />);
+        await waitFor(() => {
+            const nextBtn = screen.getByRole("button", { name: /next page/i });
+            expect(nextBtn).toBeInTheDocument();
+        });
+    });
 });
